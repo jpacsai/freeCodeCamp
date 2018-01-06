9396b6303ea94701d4ce70da379036e435d218b3
jpacsai <judit.pacsai@gmail.com>
2018-01-07 00:17:05 +0100

    adding js challanges

--- BasicJavaSript/GolfCode.js
+++ BasicJavaSript/GolfCode.js
@@ -19,3 +19,26 @@ par and strokes will always be numeric and positive.
 
 */
 
+function golfScore(par, strokes) {
+    if (strokes === 1) {
+      return "Hole-in-one!";
+    }
+    else if (strokes <= par-2 ) {
+      return "Eagle";
+    }
+    else if (strokes == par-1) {
+      return "Birdie";
+    }
+    else if (strokes == par) {
+      return "Par";
+    }
+    else if (strokes == par+1) {
+      return "Bogey";
+    }
+    else if (strokes == par+2) {
+      return "Double Bogey";
+    }
+    else {
+      return "Go Home!";
+    }
+}
