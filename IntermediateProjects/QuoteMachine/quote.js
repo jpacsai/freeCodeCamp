d0ea0a97bf6a7e3c1f485b5d839ca26ca7d7ac0c
jpacsai <judit.pacsai@gmail.com>
2018-01-28 23:44:13 +0100

    adding api

--- IntermediateProjects/QuoteMachine/quote.js
+++ IntermediateProjects/QuoteMachine/quote.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-    ('.fa-fw').on('mouseenter', (event) => {
-    $(event.currentTarget).addClass('fa-spin');
-  }).on('mouseleave', (event) => {
-    $(event.currentTarget).removeClass('fa-spin');
+    $('.generator').on('mouseenter', () => {
+    $('.fa-refresh').addClass('fa-spin');
+  }).on('mouseleave', () => {
+    $('.fa-refresh').removeClass('fa-spin');
+  });
+
+  $(".generator").on("click", function() {
+    $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+      $("#quote-content").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
+    });
   });
 });
+
+// https://quotesondesign.com/api-v4-0/
