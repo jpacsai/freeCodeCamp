d944d29d96207e5c1893333e68de29772c5ffcdc
jpacsai <judit.pacsai@gmail.com>
2018-02-04 18:48:12 +0100

    fixing ajax

--- IntermediateProjects/QuoteMachine/quote.js
+++ IntermediateProjects/QuoteMachine/quote.js
@@ -5,11 +5,27 @@ $(document).ready(function() {
     $('.fa-refresh').removeClass('fa-spin');
   });
 
-  $(".generator").on("click", function() {
-    $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-      $("#quote-content").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
+  //var quoteUrl = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback";
+  $(".generator").on("click", function (e) {
+    e.preventDefault();
+    $.ajax( {
+      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function ( data ) {
+        
+        var post = data.shift(); // The data is an array of posts. Grab the first one.
+        
+        console.log(post);
+        
+        $( '#quote-content' ).html( post.content );
+
+        // If the Source is available, use it. Otherwise hide it.
+        if ( typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined' ) {
+          $( '#quote-source' ).html('/ ' + post.title + ' / ');
+        } else {
+          $( '#quote-source' ).text( '' );
+        }
+      },
+      cache: false
     });
   });
 });
-
-// https://quotesondesign.com/api-v4-0/
