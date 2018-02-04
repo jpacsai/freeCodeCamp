6117a8ef4546025f92e17967e4995fb312e7e2a8
jpacsai <judit.pacsai@gmail.com>
2018-02-05 00:25:17 +0100

    fixing api

--- IntermediateProjects/QuoteMachine/quote.js
+++ IntermediateProjects/QuoteMachine/quote.js
@@ -5,27 +5,16 @@ $(document).ready(function() {
     $('.fa-refresh').removeClass('fa-spin');
   });
 
-  //var quoteUrl = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback";
   $(".generator").on("click", function (e) {
     e.preventDefault();
+    var wikiUrl = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
     $.ajax( {
-      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-      success: function ( data ) {
-        
-        var post = data.shift(); // The data is an array of posts. Grab the first one.
-        
-        console.log(post);
-        
-        $( '#quote-content' ).html( post.content );
-
-        // If the Source is available, use it. Otherwise hide it.
-        if ( typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined' ) {
-          $( '#quote-source' ).html('/ ' + post.title + ' / ');
-        } else {
-          $( '#quote-source' ).text( '' );
+        url: wikiUrl,
+        dataType: 'json',
+        success: function(response) {
+            //var articlelist = response[1];
+            console.log('hi');
         }
-      },
-      cache: false
-    });
-  });
+    }); 
+  }); 
 });
